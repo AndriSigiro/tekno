e6370d3d37c7ef36c7654e7b323fc6d36f354a5f
Andri sigiro <sigiroandri@gmail.com>
2025-12-02 13:52:49 +0700

    oke

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -21,9 +21,9 @@ const navItems = [
   { name: "Packages", href: "/packages", icon: Package },
   { name: "Users", href: "/users", icon: Users },
   { name: "Transactions", href: "/transactions", icon: CreditCard },
-  { name: "Settings", href: "/settings", icon: Settings },
   { name: "Vouchers", href: "/vouchers", icon: Ticket },
-  { name: "Rating", href: "/rating", icon: Star }, // Ganti icon menjadi Star
+  { name: "Rating", href: "/rating", icon: Star },
+  { name: "Settings", href: "/settings", icon: Settings },
 ];
 
 export default function Sidebar() {
